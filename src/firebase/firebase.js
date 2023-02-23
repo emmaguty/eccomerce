600f5ed16795475ea6101934f46d3f748aa7b486
emmanuelguty <manuel15901@gmail.com>
2023-02-23 15:12:46 -0600

    Se añadio firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwNHgiwc_GOJHCC-BTmiveSFO_PQMiJHU",
+  authDomain: "ecommerce-rl.firebaseapp.com",
+  projectId: "ecommerce-rl",
+  storageBucket: "ecommerce-rl.appspot.com",
+  messagingSenderId: "223837739457",
+  appId: "1:223837739457:web:c5ff95199eb7fb684f3740",
+  measurementId: "G-X0601QCKSN"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
