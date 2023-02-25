dcf04e29d9d343a99d3b94831053f5a0b7642541
emmanuelguty <manuel15901@gmail.com>
2023-02-25 16:54:10 -0600

    El boton de pay se le agrego la suma total de productos

--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -1,17 +1,65 @@
 import * as React from 'react';
 
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import { Button, Divider, Typography } from '@mui/material';
+
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 import Review from './Review';
+import { getBasketTotal } from '../../reducer';
+import { useStateValue } from '../../stateProvider';
 
-export default function PaymentForm({backStep, nextStep}) {
+import {accouting} from 'accounting';
+
+const stripePromise = loadStripe("pk_test_51MfUQRI8refubrkfUd6JucgcIpbiweA9LUIWGyJD7hYVqJoudMjaLD7cISxviueSMhpWlZlXR4QHlHkgeNPAm8Qd00y4OQ7tHl");
+
+const CARD_ELEMENT_OPTIONS = {
+  iconStyle: "solid",
+  hidePostalCode: true,
+  style: {
+    base: {
+      iconColor: "rgb(230, 57, 122)",
+      color: "#333",
+      fontSize: "18px",
+      "::placeholder": {
+        color: "#ccc"
+      },
+    },
+    invalid: {
+      color: "#e5424d",
+      ":focus": {
+        color: "#303238"
+      }
+    }
+  }
+}
+
+const CheckoutForm = ({ backStep, nextStep }) => {
+
+  const [{basket}, dispatch] = useStateValue();
+
+  return (
+    <form>
+      <CardElement options={CARD_ELEMENT_OPTIONS} />
+      <div style={{display: "flex", justifyContent: "space-between", marginTop: "1rem"}}>
+        <Button variant="outlined" onClick={backStep}>Back</Button>
+        <Button type="submit" variant="outlined" color="primary" disabled={true} > `Pay ${getBasketTotal(basket)}`</Button>
+      </div>
+    </form>
+  )
+}
+
+export default function PaymentForm({ backStep, nextStep }) {
   return (
     <React.Fragment>
       <Review />
+      <Divider />
+      <Typography variant="h6" gutterBottom style={{ margin: "20px 0" }}>
+        Payment method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm backStep={backStep} nextStep={nextStep} />
+      </Elements>
     </React.Fragment>
   );
 }
