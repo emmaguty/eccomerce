1142173c1102da2cd88b6c98e1ddb029ae16bcf1
emmanuelguty <manuel15901@gmail.com>
2023-02-25 17:53:12 -0600

    Se realizo una prueba de una tarjeta de credito

--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -9,7 +9,7 @@ import Review from './Review';
 import { getBasketTotal } from '../../reducer';
 import { useStateValue } from '../../stateProvider';
 
-import {accouting} from 'accounting';
+import { accouting } from 'accounting';
 
 const stripePromise = loadStripe("pk_test_51MfUQRI8refubrkfUd6JucgcIpbiweA9LUIWGyJD7hYVqJoudMjaLD7cISxviueSMhpWlZlXR4QHlHkgeNPAm8Qd00y4OQ7tHl");
 
@@ -36,14 +36,27 @@ const CARD_ELEMENT_OPTIONS = {
 
 const CheckoutForm = ({ backStep, nextStep }) => {
 
-  const [{basket}, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement)
+    })
+    if (!error) {
+      const { id } = paymentMethod;
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <CardElement options={CARD_ELEMENT_OPTIONS} />
-      <div style={{display: "flex", justifyContent: "space-between", marginTop: "1rem"}}>
+      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "1rem" }}>
         <Button variant="outlined" onClick={backStep}>Back</Button>
-        <Button type="submit" variant="outlined" color="primary" disabled={true} > `Pay ${getBasketTotal(basket)}`</Button>
+        <Button type="submit" variant="contained" color="primary" disabled={false} > Pay ${getBasketTotal(basket)}</Button>
       </div>
     </form>
   )
@@ -63,56 +76,3 @@ export default function PaymentForm({ backStep, nextStep }) {
     </React.Fragment>
   );
 }
-
-{/* <Typography variant="h6" gutterBottom>
-Payment method
-</Typography>
-<Grid container spacing={3}>
-<Grid item xs={12} md={6}>
-  <TextField
-    required
-    id="cardName"
-    label="Name on card"
-    fullWidth
-    autoComplete="cc-name"
-    variant="standard"
-  />
-</Grid>
-<Grid item xs={12} md={6}>
-  <TextField
-    required
-    id="cardNumber"
-    label="Card number"
-    fullWidth
-    autoComplete="cc-number"
-    variant="standard"
-  />
-</Grid>
-<Grid item xs={12} md={6}>
-  <TextField
-    required
-    id="expDate"
-    label="Expiry date"
-    fullWidth
-    autoComplete="cc-exp"
-    variant="standard"
-  />
-</Grid>
-<Grid item xs={12} md={6}>
-  <TextField
-    required
-    id="cvv"
-    label="CVV"
-    helperText="Last three digits on signature strip"
-    fullWidth
-    autoComplete="cc-csc"
-    variant="standard"
-  />
-</Grid>
-<Grid item xs={12}>
-  <FormControlLabel
-    control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-    label="Remember credit card details for next time"
-  />
-</Grid>
-</Grid> */}
